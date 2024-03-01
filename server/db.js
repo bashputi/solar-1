a3a85dbc0cb34a7250b2c1c481ce118f70d36d4e
Rime <rimeislam672@gmail.com>
2024-03-01 10:42:01 +0600

    server

--- server/db.js
+++ server/db.js
@@ -2,7 +2,11 @@ const {Pool} = require("pg");
 const { database } = require("pg/lib/defaults");
 
 const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL ,
-  })
+  host: "localhost",
+  user: "postgres",
+  port: 4000,
+  password: '123pailamatabiri',
+  database: "authdb"
+});
 
 module.exports = pool;
